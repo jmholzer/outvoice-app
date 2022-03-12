056b3fa7257a98c82caae6bbf76d817e84bff61b
jmholzer <jannic.holzer@gmail.com>
2022-03-12 19:43:41 +0100

    Add tab panel component.

---
+++ src/workflowTabs.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import InvoiceForm from './invoiceGeneration'
+//import About from './About'
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function WorkflowTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <div style={{ overflow: "scroll" }}>
+        <AppBar position="static" style={{ backgroundColor: '#3B97D3' }}>
+          <Tabs value={value} onChange={handleChange} centered TabIndicatorProps={{style: {backgroundColor: '#FFFFFF'}}}>
+            <Tab label="SEND AN INVOICE" {...a11yProps(0)} />
+            <Tab label="USER GUIDE" {...a11yProps(1)} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0} >
+          <InvoiceForm />
+        </TabPanel>
+
+      </div>
+    </div>
+  );
+}
