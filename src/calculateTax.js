ce2d38ad6791f0b6fe84887eaab666c9906fa66b
jmholzer <jannic.holzer@gmail.com>
2022-04-01 04:40:37 +0200

    Add tax calculation display.

---
+++ src/calculateTax.js
@@ -0,0 +1,5 @@
+function calculateTax(subtotal) {
+    return 0.2 * subtotal;
+}
+
+export { calculateTax }
--- src/totalSum.js
+++ src/totalSum.js
@@ -3,6 +3,7 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { currencyFormatter } from './currencyFormatter.js';
 import { calculateTotal } from './calculateTotal.js';
+import { calculateTax } from './calculateTax.js'
 
 export default function TotalSum(props) {
 
@@ -12,14 +13,24 @@ export default function TotalSum(props) {
         setRowData(props.rowData);
     }, [props.rowData]);
 
+    var subtotal = calculateTotal(rowData);
+    var tax = calculateTax(subtotal);
+    var total = subtotal + tax;
+
     return (
         <Typography 
             inline variant="subtitle1"
             align="right"
             style={{ 'paddingRight': '60px' }}
             >
+                <Box fontSize = {14} fontStyle="oblique" m={1}>
+                    Subtotal – {currencyFormatter(subtotal, "£")}
+                </Box>
+                <Box fontSize = {14} fontStyle="oblique" m={1}>
+                    VAT – {currencyFormatter(tax, "£")}
+                </Box>
                 <Box fontSize = {20} fontStyle="oblique" m={1}>
-                    Total – {currencyFormatter(calculateTotal(rowData), "£")}
+                    Total – {currencyFormatter(total, "£")}
                 </Box>
         </Typography>
     );
