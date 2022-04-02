7779c3254708679b205a4bef9ba47cb536d78acb
jmholzer <jannic.holzer@gmail.com>
2022-04-02 03:24:25 +0200

    Remove logging line from calculateTotal.js

--- src/calculateTotal.js
+++ src/calculateTotal.js
@@ -1,7 +1,5 @@
 function calculateTotal(rowData) {
     var result = 0;
-    //console.log(rowData);
-    console.log(typeof(rowData))
     for (let row of rowData) {
         result += row.cost_per_item * row.count 
     }
